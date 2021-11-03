d3e33c3050ea2f65d53b7bdbaf199789df3d0f90
ilya rudenko <webilya29@gmail.com>
2021-11-03 18:51:34 +0300

    Implement homework

--- Sources/CodingStyle/CodingStyle.swift
+++ Sources/CodingStyle/CodingStyle.swift
@@ -1,6 +1,112 @@
-public struct CodingStyle {
-    public private(set) var text = "Hello, World!"
+enum Style {
+    case camelCase, snakeCase, kebabCase
+}
 
-    public init() {
+///snake_case_example
+///CamelCaseExample
+///kebab-case-example
+@propertyWrapper struct CodingStyle {
+    private var storedString = ""
+    
+    var wrappedValue: String {
+        get {
+            return transformStringToCase()
+        }
+        set {
+            storedString = newValue
+        }
     }
+    
+    private var style: Style
+    
+    init(wrappedValue: String, style: Style) {
+        self.storedString = wrappedValue
+        self.style = style
+    }
+    
+    private func transformStringToCase() -> String {
+        switch style {
+        case .camelCase:
+            return stringToCamelCase()
+        case .snakeCase:
+            return stringToSnakeCase()
+        case .kebabCase:
+            return stringToKebabCase()
+        }
+    }
+    
+    private func stringToCamelCase() -> String {
+        return storedString
+            .split(whereSeparator: { $0.isPunctuation || $0.isWhitespace })
+            .map { $0.lowercased().firstUppercased  }
+            .joined()
+    }
+    
+    private func stringToSnakeCase() -> String {
+        return splittingWords().joined(separator: "_")
+    }
+    
+    private func stringToKebabCase() -> String {
+        return splittingWords().joined(separator: "-")
+    }
+    
+    private func splittingWords() -> [String] {
+        var result: [String] = []
+        
+        let splitWords = storedString.split(whereSeparator: { $0.isPunctuation || $0.isWhitespace })
+        
+        for word in splitWords {
+            if word.count <= 1 {
+                result.append(word.lowercased())
+                continue
+            }
+            
+            let countChars = word.count
+            var indices: [Int] = []
+            
+            for (index, item) in word.enumerated() where item.isUppercase {
+                indices.append(index)
+            }
+            
+            let countUppercase = indices.count
+            
+            if countUppercase > 1 && countUppercase < countChars {
+                for index in 0..<countUppercase {
+                    let startIndex = indices[index]
+                    let endIndex: Int
+                    
+                    if index+1 >= countUppercase {
+                        endIndex = word.count
+                    } else {
+                        endIndex = indices[index+1]
+                    }
+                    
+                    result.append(word[startIndex..<endIndex].lowercased())
+                }
+            } else {
+                result.append(word.lowercased())
+            }
+        }
+        
+        return result
+    }
+    
+}
+
+extension StringProtocol {
+    var firstUppercased: String { prefix(1).uppercased() + dropFirst() }
+}
+
+extension String.SubSequence {
+    subscript (index: Int) -> Character {
+        let charIndex = self.index(self.startIndex, offsetBy: index)
+        return self[charIndex]
+    }
+    
+    subscript (range: Range<Int>) -> Substring {
+        let startIndex = self.index(self.startIndex, offsetBy: range.startIndex)
+        let stopIndex = self.index(self.startIndex, offsetBy: range.startIndex + range.count)
+        return self[startIndex..<stopIndex]
+    }
+    
 }
--- Tests/CodingStyleTests/CodingStyleTests.swift
+++ Tests/CodingStyleTests/CodingStyleTests.swift
@@ -2,10 +2,59 @@ import XCTest
 @testable import CodingStyle
 
 final class CodingStyleTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CodingStyle().text, "Hello, World!")
+    
+    ///snake_case_example
+    ///CamelCaseExample
+    ///kebab-case-example
+    
+    func testCamelCase() throws {
+        @CodingStyle(style: .camelCase) var camelCaseText: String = ""
+
+        camelCaseText = "_thIs-iS text"
+        XCTAssertEqual(camelCaseText, "ThisIsText")
+        camelCaseText = "some-text_pe_qwe-Qweqe"
+        XCTAssertEqual(camelCaseText, "SomeTextPeQweQweqe")
+        camelCaseText = "Some Text"
+        XCTAssertEqual(camelCaseText, "SomeText")
+        camelCaseText = " some_text"
+        XCTAssertEqual(camelCaseText, "SomeText")
+        camelCaseText = "SOME TEXT"
+        XCTAssertEqual(camelCaseText, "SomeText")
+        camelCaseText = "-Some text"
+        XCTAssertEqual(camelCaseText, "SomeText")
+        camelCaseText = "Some-Text"
+        XCTAssertEqual(camelCaseText, "SomeText")
+        camelCaseText = "Some---_Text"
+        XCTAssertEqual(camelCaseText, "SomeText")
+        camelCaseText = "kebab-style_snake_case"
+        XCTAssertEqual(camelCaseText, "KebabStyleSnakeCase")
+    }
+    
+    func testSnakeCase() throws {
+        @CodingStyle(style: .snakeCase) var snakeCaseText: String = ""
+        
+        snakeCaseText = "FooBar_FOOBAR-foo-bar"
+        XCTAssertEqual(snakeCaseText, "foo_bar_foobar_foo_bar")
+        snakeCaseText = "foo-bar"
+        XCTAssertEqual(snakeCaseText, "foo_bar")
+        snakeCaseText = "foo_bar"
+        XCTAssertEqual(snakeCaseText, "foo_bar")
+        snakeCaseText = "FooBarFooBar-foo bar"
+        XCTAssertEqual(snakeCaseText, "foo_bar_foo_bar_foo_bar")
+        snakeCaseText = "fo _    -bar FOOBAR-Foo BAR"
+        XCTAssertEqual(snakeCaseText, "fo_bar_foobar_foo_bar")
+        snakeCaseText = "F-B"
+        XCTAssertEqual(snakeCaseText, "f_b")
+    }
+    
+    func testKebabCase() throws {
+        @CodingStyle(style: .kebabCase) var kebabCaseText: String = ""
+        
+        kebabCaseText = "FooBar_FOOBAR-foo-bar"
+        XCTAssertEqual(kebabCaseText, "foo-bar-foobar-foo-bar")
+        kebabCaseText = "foo_bar"
+        XCTAssertEqual(kebabCaseText, "foo-bar")
+        kebabCaseText = "foo bar BAR"
+        XCTAssertEqual(kebabCaseText, "foo-bar-bar")
     }
 }
